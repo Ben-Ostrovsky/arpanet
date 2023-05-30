0ed697592ca208a86ad00b9e6a0d700b72b2ad9a
Ben Ostrovsky <ben.leo.ost@gmail.com>
2023-05-29 22:13:11 -0700

    HC

--- src/pages/historicalContext.tsx
+++ src/pages/historicalContext.tsx
@@ -3,7 +3,7 @@ import {
   VerticalTimeline,
   VerticalTimelineElement,
 } from "react-vertical-timeline-component";
-import { Text } from "@nextui-org/react";
+import { Text, Image, Spacer } from "@nextui-org/react";
 import "react-vertical-timeline-component/style.min.css";
 
 export default function About() {
@@ -27,15 +27,23 @@ export default function About() {
 
   return (
     <div>
-      <VerticalTimeline lineColor="#0072F5">
+      <VerticalTimeline lineColor="#697177">
         <VerticalTimelineElement
           contentStyle={styles}
           contentArrowStyle={{ borderRight: "10px solid " + styles.background }}
           iconStyle={{ background: "#0072F5" }}
           date="October 4th, 1957"
         >
-          <Text h3> Exigence</Text>
-          <Text weight={"thin"}>
+          <Text h3> Sputnik 1 - Exigence</Text>
+          <Spacer />
+          <Image
+            src={
+              "https://www.nasa.gov/sites/default/files/images/190844main_GPN-2002-000166_full.jpg"
+            }
+            width={350}
+          />
+          <Spacer />
+          <Text span>
             The Soviet Union launches the satellite Sputnik, marking the
             beginning of the Space Age and triggering the Space Race between the
             United States and the Soviet Union. The launch of Sputnik raises
@@ -51,7 +59,7 @@ export default function About() {
           date="1958"
         >
           <Text h3> The Beginning</Text>
-          <Text weight={"thin"}>
+          <Text span>
             In response to the Soviet Union's advancements in space technology,
             the United States creates the Advanced Research Projects Agency
             (ARPA) within the Department of Defense. ARPA's mission is to foster
@@ -67,7 +75,7 @@ export default function About() {
           date="1962"
         >
           <Text h3> Early Days</Text>
-          <Text weight={"thin"}>
+          <Text span>
             J.C.R. Licklider becomes the first director of ARPA's Information
             Processing Techniques Office (IPTO). Licklider envisions a "galactic
             network" that connects computers and facilitates collaboration among
@@ -82,12 +90,13 @@ export default function About() {
           date="1965-1967"
         >
           <Text h3> ARPANET</Text>
-          <Text weight={"thin"}>
+          <Text span>
             Lawrence Roberts, an ARPA scientist, publishes a plan for a computer
             network that would allow researchers to share and access resources
             remotely. This plan becomes the blueprint for ARPANET.
           </Text>
-          <Text>
+          <Spacer />
+          <Text span>
             Soon, ARPA issues a Request for Quotation (RFQ) to build ARPANET.
             Several institutions, including Bolt, Beranek and Newman (BBN),
             submit proposals.{" "}
@@ -101,7 +110,7 @@ export default function About() {
           date="1967-1969"
         >
           <Text h3> Node One</Text>
-          <Text>
+          <Text span>
             Soon, The first ARPANET node is established when BBN successfully
             connects the computers at the University of California, Los Angeles
             (UCLA) and the Stanford Research Institute (SRI). This connection is
@@ -112,15 +121,56 @@ export default function About() {
         <VerticalTimelineElement
           contentStyle={styles}
           contentArrowStyle={{ borderRight: "10px solid " + styles.background }}
-          iconStyle={{ background: "#0072F5" }}
-          date="1967-1969"
+          iconStyle={{ background: "#9750DD          " }}
+          date="1971"
         >
-          <Text h3> Node One</Text>
+          <Text h3> Information Sharing and Collaboration</Text>
+          <Text span>
+            Researchers at the University of Southern California (USC) and
+            Stanford University used ARPANET to collaborate on the development
+            of the ARPANET Network Measurement Center (NMC).
+          </Text>
           <Text>
-            Soon, The first ARPANET node is established when BBN successfully
-            connects the computers at the University of California, Los Angeles
-            (UCLA) and the Stanford Research Institute (SRI). This connection is
-            the birth of ARPANET, the precursor to the modern Internet.
+            "The ARPANET project has successfully shown the feasibility of a
+            network of computers, connected to one another by medium- and
+            high-speed communication links." - Lawrence Roberts, ARPA scientist,
+            in a 1967 paper.
+          </Text>
+        </VerticalTimelineElement>
+
+        <VerticalTimelineElement
+          contentStyle={styles}
+          contentArrowStyle={{ borderRight: "10px solid " + styles.background }}
+          iconStyle={{ background: "#9750DD          " }}
+          date="1972"
+        >
+          <Text h3> Email: revolutionizing long-distance communication</Text>
+          <Text span>
+            Ray Tomlinson sent the first email message between two
+            ARPANET-connected computers, marking a significant milestone in
+            electronic communication.
+          </Text>
+          <Text>
+            "The @ sign seemed to make sense. I used it to indicate that the
+            user was 'at' some other host rather than being local." - Ray
+            Tomlinson, on choosing the @ symbol for email addresses.
+          </Text>
+        </VerticalTimelineElement>
+
+        <VerticalTimelineElement
+          contentStyle={styles}
+          contentArrowStyle={{
+            borderRight: "10px solid " + styles.background,
+          }}
+          iconStyle={{ background: "#0072F5" }}
+          date="1974"
+        >
+          <Text h3> Going Public </Text>
+          <Text span>
+            The first public demonstration of ARPANET takes place, showcasing
+            the potential of networked computing. This demonstration sparks
+            interest and excitement about the possibilities of interconnected
+            computers.
           </Text>
         </VerticalTimelineElement>
       </VerticalTimeline>
